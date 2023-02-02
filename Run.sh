7ee621c580ba858c4b0e8d4e5e5ab30fde3bae42
ACTOMtoolbox <120719451+ACTOMtoolbox@users.noreply.github.com>
2023-02-02 12:25:48 +0000

    Add files via upload

---
+++ Run.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+TIMESTAMP=$(date +%Y%m%d%H%M%S)
+title=Run-$TIMESTAMP
+
+mkdir -p $title
+cd $title
+
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
+
+rm -r Run-All.bat
+sh Run-All.sh
+mkdir -p logs
+mv Run-All.sh logs/Run-All.sh
