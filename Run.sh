afd2b8e42e6d0ccd5e912a8e8c97b2b89c3272f5
ACTOMtoolbox <120719451+ACTOMtoolbox@users.noreply.github.com>
2023-02-03 09:48:17 +0000

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -5,7 +5,7 @@ title=Run-$TIMESTAMP
 mkdir -p $title
 cd $title
 
-docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=unix$DISPLAY --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
 
 rm -r Run-All.bat
 sh Run-All.sh
