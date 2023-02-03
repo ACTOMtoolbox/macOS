3656905f638c4c47b6ab9eb9c119bee6b57a75d5
ACTOMtoolbox <120719451+ACTOMtoolbox@users.noreply.github.com>
2023-02-03 10:44:23 +0000

    Add files via upload

--- Run.sh
+++ Run.sh
@@ -4,8 +4,10 @@ title=Run-$TIMESTAMP
 
 mkdir -p $title
 cd $title
+IP=$(/usr/sbin/ipconfig getifaddr en0)
+/opt/X11/bin/xhost + "$IP"
 
-docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY=$DISPLAY --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="${IP}:0" --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
 
 rm -r Run-All.bat
 sh Run-All.sh
