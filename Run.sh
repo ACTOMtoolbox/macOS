b11963297b941d1641caeee4878cb92a37bb8311
ACTOMtoolbox <120719451+ACTOMtoolbox@users.noreply.github.com>
2023-02-06 10:09:49 +0000

    Update Run.sh

--- Run.sh
+++ Run.sh
@@ -7,7 +7,7 @@ cd $title
 IP=$(/usr/sbin/ipconfig getifaddr en0)
 /opt/X11/bin/xhost + "$IP"
 
-docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="${IP}:0" --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
+docker run -it -v /tmp/.X11-unix:/tmp/.X11-unix -e DISPLAY="${IP}:0" -e systemOS=mac --mount type=bind,source="$(pwd)",target=/srv/actom-run/input actomtoolbox/actom-run
 
 rm -r Run-All.bat
 sh Run-All.sh
